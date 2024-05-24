eca9cc0aad7bd6b011629bca993dbd8456c8d3ab
sum130 <sm3904@naver.com>
2024-05-24 22:02:53 +0900

    fix: fixing passthrough
    패스스루 적용

--- Assets/Scripts/cshVRuser.cs
+++ Assets/Scripts/cshVRuser.cs
@@ -13,35 +13,36 @@ public class cshVRuser : MonoBehaviourPun
     {
         if (!photonView.IsMine)
         {
-            DestroyImmediate(GetComponentInChildren<OVRManager>()); 
-            
+            DestroyImmediate(GetComponentInChildren<OVRManager>());
+            DestroyImmediate(GameObject.Find("[BuildingBlock] Room Model"));
         }
     }
     void Start()
     {
-        /*if (!photonView.IsMine)
+        if (!photonView.IsMine)
         {
-            // VR 카메라 FALSE
-            //GetComponentInChildren<OVRCameraRig>().disableEyeAnchorCameras = true;
+            
+            //// VR 카메라 FALSE
+            ////GetComponentInChildren<OVRCameraRig>().disableEyeAnchorCameras = true;
 
-            Camera[] cameras;
-            cameras = transform.gameObject.GetComponentsInChildren<Camera>();
-            foreach (Camera c in cameras)
-            {
-                c.enabled = false;
-            }
-            //다른 사용자들에게 나의 첫번째 자식(= canvas)을 보지 못하게 false
-            transform.GetChild(0).gameObject.SetActive(false);
+  //          //Camera[] cameras;
+            //cameras = transform.gameObject.GetComponentsInChildren<Camera>();
+            //foreach (Camera c in cameras)
+            //{
+            //    c.enabled = false;
+            //}
+            ////다른 사용자들에게 나의 첫번째 자식(= canvas)을 보지 못하게 false
+            //transform.GetChild(0).gameObject.SetActive(false);
 
-            // VR 사용자가 네트워크 상에서 자신이 아닐 경우
-            // 오큘러스 충돌을 방지하기 위해 관련된 속성들을 모두 비활성화
-            //GetComponent<CharacterController>().enabled = false;
-            //GetComponent<OVRPlayerController>().enabled = false;
-            //GetComponentInChildren<OVRCameraRig>().enabled = false;
-            //GetComponentInChildren<OVRManager>().enabled = false;
-            //GetComponentInChildren<OVRHeadsetEmulator>().enabled = false;
-            Debug.Log("Camera false");
-        }*/
+  //          //// VR 사용자가 네트워크 상에서 자신이 아닐 경우
+            //// 오큘러스 충돌을 방지하기 위해 관련된 속성들을 모두 비활성화
+            ////GetComponent<CharacterController>().enabled = false;
+            ////GetComponent<OVRPlayerController>().enabled = false;
+            ////GetComponentInChildren<OVRCameraRig>().enabled = false;
+            ////GetComponentInChildren<OVRManager>().enabled = false;
+            ////GetComponentInChildren<OVRHeadsetEmulator>().enabled = false;
+            //Debug.Log("Camera false");
+        }
     }
 
     // Update is called once per frame
