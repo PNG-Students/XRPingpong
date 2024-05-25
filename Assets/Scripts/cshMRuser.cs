cfa203c516281dbbe6fe4aad9f8faa3a458dbc94
sum130 <sm3904@naver.com>
2024-05-25 20:35:47 +0900

    공, vr위치 지정_테스트필요

--- Assets/Scripts/cshMRuser.cs
+++ Assets/Scripts/cshMRuser.cs
@@ -8,6 +8,7 @@ public class cshMRuser : MonoBehaviourPun
 {
     public GameObject tablePrefab;
     private GameObject roomTable;
+    public GameObject ballPrefab;
 
     private void Awake()
     {
@@ -53,13 +54,16 @@ public class cshMRuser : MonoBehaviourPun
 
         // Find the roomTable object
         GameObject roomTable = GameObject.Find("TableVolume(Clone)");
+        GameObject ballPos = GameObject.Find("BallSpawnPos");
         if (roomTable != null)
         {
             // Instantiate the tablePrefab using PhotonNetwork
             GameObject instantiatedTable = PhotonNetwork.Instantiate(tablePrefab.name, roomTable.transform.position, roomTable.transform.rotation);
+            GameObject instantiatedBall = PhotonNetwork.Instantiate(ballPrefab.name, ballPos.transform.position, ballPos.transform.rotation);
 
             // Set the scale of the instantiated table to match the roomTable
-            instantiatedTable.transform.Find("Parent").localScale = roomTable.transform.localScale;
+            instantiatedTable.transform.localScale = roomTable.transform.Find("Parent").localScale;
+
             //instantiatedTable.transform.localScale = roomTable.transform.localScale;
 
             Debug.Log("2 seconds have passed, table instantiated and transformed.");
