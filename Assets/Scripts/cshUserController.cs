40c6c4357797f412efe41d371c56c61b0e797844
sum130 <sm3904@naver.com>
2024-05-29 14:19:49 +0900

    fix: Character

--- Assets/Scripts/cshUserController.cs
+++ Assets/Scripts/cshUserController.cs
@@ -8,14 +8,14 @@ public class cshUserController : MonoBehaviourPun
     private Transform cameraRig; // 카메라 리그
     private Transform rightHandRig; // 카메라 리그
     public Transform rightHandModel;
-    public Transform characterModel; // 캐릭터 모델
+    public GameObject characterModel; // 캐릭터 모델
 
     public float sensitivity = 1f; // 카메라 민감도
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterModel.SetActive(false);
         
     }
 
@@ -31,8 +31,8 @@ public class cshUserController : MonoBehaviourPun
             Vector3 cameraRotation = cameraRig.rotation.eulerAngles;
 
             // 캐릭터 모델의 회전 값 업데이트
-            characterModel.rotation = Quaternion.Euler(0f, cameraRotation.y, 0f);
-            characterModel.position = cameraRig.position + new Vector3(0, -0.3f, 0);
+            characterModel.transform.rotation = Quaternion.Euler(0f, cameraRotation.y, 0f);
+            characterModel.transform.position = cameraRig.position + new Vector3(0, -0.3f, 0);
 
             // 카메라의 회전 값
             Vector3 rightHandRotation = rightHandRig.rotation.eulerAngles;
